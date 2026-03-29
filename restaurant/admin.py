a00d76a71fb86c1813933a746720cde08b4ae906
Geoffrey Edmund Moraes <geoff.moraes@gmail.com>
2026-03-29 10:49:41 +0530

    Add Menu and Booking models with migrations and admin registration

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Menu, Booking
 
-# Register your models here.
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'inventory')
+    search_fields = ('title',)
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'booking_date', 'no_of_guests')
+    list_filter = ('booking_date',)
+    search_fields = ('name',)
--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.SmallIntegerField()
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return f'{self.title} : {str(self.price)}'
+
+
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField()
+    booking_date = models.DateField()
+
+    class Meta:
+        ordering = ['booking_date', 'name']
+
+    def __str__(self):
+        return f'{self.name} - {self.booking_date} ({self.no_of_guests} guests)'
